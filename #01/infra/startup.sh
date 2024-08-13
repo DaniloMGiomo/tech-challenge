2227da5b642a4d8a3397c4d0a0dbdd4af56d3950
dmgiomo <131303220+dmgiomo@users.noreply.github.com>
2024-08-13 19:08:30 -0300

    ajust path

--- #01/infra/startup.sh
+++ #01/infra/startup.sh
@@ -1,13 +1,6 @@
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt update
-
-
-sudo apt install docker -y
-sudo apt update
-
+#!/bin/bash
+sudo dnf update -y
+sudo dnf install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
@@ -17,5 +10,14 @@ newgrp docker
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-docker build -t fastAPI-service .
-docker run -d -p 8000:8000 fastAPI-service
+# portainer
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# github
+sudo dnf update
+sudo dnf install git -y
+
+# tech-challenge clone
+git clone https://github.com/DaniloMGiomo/tech-challenge.git
+
